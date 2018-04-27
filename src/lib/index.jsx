7fd984c2b7a50261d0a983359ab1d6f7c4d4f3b8
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-04-27 13:33:11 +0800

    Initial component port

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,18 +1,60 @@
-import React, { Component } from "react";
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import Rellax from 'rellax'
 
-class MySuperCoolComponent extends Component {
-	handleClick = () => {
-		console.log("Click!");
-	};
+export default class Parallax extends PureComponent {
+	componentDidMount() {
+		if (this.el) {
+			const { centered, horizontal, onMove } = this.props
+			this.rellax = new Rellax(this.el, {
+				centered,
+				horizontal,
+				callback: onMove
+			})
+		}
+	}
+
+	componentWillUnmount() {
+		if (this.rellax) {
+			this.rellax.destroy()
+		}
+	}
 
 	render() {
-		const { color, children } = this.props;
+		const { children, className, percentage, speed, zIndex } = this.props
+
 		return (
-			<button onClick={this.handleClick} style={{ color }}>
+			<div
+				className={className}
+				ref={el => { this.el = el }}
+				data-rellax-percentage={percentage}
+				data-rellax-speed={speed}
+				data-rellax-zindex={zIndex}
+			>
 				{children}
-			</button>
-		);
+			</div>
+		)
 	}
 }
 
-export default MySuperCoolComponent;
+Parallax.propTypes = {
+	centered: PropTypes.bool,
+	children: PropTypes.node,
+	className: PropTypes.string,
+	horizontal: PropTypes.bool,
+	onMove: PropTypes.func,
+	percentage: PropTypes.number,
+	speed: PropTypes.number,
+	zIndex: PropTypes.number
+}
+
+Parallax.defaultProps = {
+	centered: false,
+	children: null,
+	className: null,
+	horizontal: false,
+	onMove: null,
+	percentage: 0,
+	speed: 0,
+	zIndex: 0
+}
